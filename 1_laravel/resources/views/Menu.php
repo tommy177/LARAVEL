355f33d07e0f38e5e0fe24a3bbc3dbf4df08dc1a
tommy177 <84911068+tommy177@users.noreply.github.com>
2022-09-22 23:09:19 +0300

    1_laravel
    href не получилось

---
+++ 1_laravel/resources/views/Menu.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+Welcome <br>
+<a href="/../laravel/resources/views/Project.php">Проект</a>
+<a href="News.php">Новость</a>
+<a href="./Timeline.php">Лента новостей</a>
+Что я тут сделал не так ?
+
+</body>
+</html>
